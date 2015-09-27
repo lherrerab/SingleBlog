60387d92dc641238b25d7ffe9c1cf3b6f1dd3faa
Leonardo Herrera <herreraleonardo@hotmail.com>
2015-09-27 18:15:12 -0500

    Update blog_model.php

--- application/models/blog_model.php
+++ application/models/blog_model.php
@@ -1,33 +1,40 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
 	
-	class blog_model extends CI_Model{
+	class blog_model extends CI_Model
+	{
 		
-		function __construct(){
+		function __construct()
+		{
 			parent::__construct();
 			$this->load->library('session');
 		}
 		
-		function findUser($data){
+		function findUser($data)
+		{
 			$user = $this->mongo_db->get_where('users',$data);			
 			return $user;
 		}
 		
-		function getPosts(){			
+		function getPosts()
+		{			
 			$posts = $this->mongo_db->order_by(array('posted_on' => 'DESC'))->get('posts');
 			return $posts;
 		}
 		
-		function savePost($data){
+		function savePost($data)
+		{
 			$id = $this->mongo_db->insert('posts',$data);
 		}
 		
-		function getPost($id){
+		function getPost($id)
+		{
 			$post = $this->mongo_db->get_where('posts', array('_id' => new MongoId($id)));
 			return $post[0];
 		}
 		
-		function saveComment($id,$comment){
+		function saveComment($id,$comment)
+		{
 			$post = $this->mongo_db->get_where('posts', array('_id' => new MongoId($id)));
 			$comments = (isset($post[0]['comments'])) ? $post[0]['comments']: array();
 			
@@ -43,17 +50,21 @@
 			$this->mongo_db->where(array('_id'=> new MongoId($id)))->set(array('comments' => $comments))->update('posts');				
 		}
 		
-		function editPost($id,$data){
+		function editPost($id,$data)
+		{
 			$this->mongo_db->where(array('_id'=> new MongoId($id)))->set($data)->update('posts');
 		}
 		
-		function deletePost($id){
+		function deletePost($id)
+		{
 			$this->mongo_db->where(array('_id'=> new MongoId($id)))->delete('posts');
 		}
 		
-		function isLoggedin(){
-			if(empty($this->session->userdata('username')))
+		function isLoggedin()
+		{
+			if(empty($this->session->userdata('username'))){
 				return false;
+			}
 			else {
 				return true;
 			}
