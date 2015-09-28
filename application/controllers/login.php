2b807ce58d07f563f87299d900d1dddbf863bb77
Leonardo Herrera <herreraleonardo@hotmail.com>
2015-09-28 11:06:49 -0500

    Update login.php

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,41 +2,48 @@
 	defined('BASEPATH') OR exit('No direct script access allowed');
 	require_once('blog.php');
 	
-    class Login extends CI_Controller{
+    	class Login extends CI_Controller
+    	{
     	
-    	function __construct(){
+    		function __construct()
+    		{
 			parent::__construct();
 			$this->load->model('blog_model');			
 		}
 		
 		public function index(){
-			if ($this->blog_model->isLoggedin()) {
+			if ($this->blog_model->isLoggedin()) 
+			{
 				redirect('blog/viewDashboard'); 
 			}		
 			$this->load->view('login');			
 		}
 		
-		public function checkCredentials(){
+		public function checkCredentials()
+		{
 			$data = array(
-							"username" => $this->input->post('username'),
-							"password" => md5($this->input->post('password'))
-						);								
+					"username" => $this->input->post('username'),
+					"password" => md5($this->input->post('password'))
+				);								
 			$user = $this->blog_model->findUser($data);	
-			
-			if(empty($user)){
+		
+			if(empty($user))
+			{
 				$data['msgLogin'] = "Username/Password incorrect";
 				$this->load->view('login',$data);
 			}		
-			else{				
+			else
+			{				
 				$this->session->set_userdata($user[0]);
 				redirect('blog/viewDashboard');
+				}
 			}
-		}
 		
-		public function logout(){
+		public function logout()
+		{
 			$this->session->sess_destroy();
 			redirect("/blog/");
 		}
 				
-    }
+    	}
 ?>
